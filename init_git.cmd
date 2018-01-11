71f4619d3afe1b2c5a6413211c3ef081d291e64b
Angular CLI <angular-cli@angular.io>
2018-01-11 14:01:56 +0100

    chore: initial commit from @angular/cli

---
+++ init_git.cmd
@@ -0,0 +1,6 @@
+echo "# ludwig-recipe-app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/darkzero1983/ludwig-recipe-app.git
+git push -u origin master
