03af29b6da5afd875707304db6e91e2a87a209fe
Peter Ludwig <peter.ludwig@smarthouse.de>
2018-01-12 18:00:04 +0100

    CHANGED: fix pwa

--- src/app/modules/recipe/recipe.module.ts
+++ src/app/modules/recipe/recipe.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { RecipeOverviewComponent } from './components/overview/recipe.overview.component';
-
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
@@ -20,6 +18,6 @@ import { ModalModule } from 'ngx-bootstrap/modal';
   ],
   exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [],
-  bootstrap: [RecipeOverviewComponent]
+  bootstrap: []
 })
 export class RecipeModule { }
--- src/app/modules/recipe_module/components/index.ts
+++
@@ -1,1 +0,0 @@
-import { RecipeOverviewComponent } from "./overview/recipe.overview.component";
