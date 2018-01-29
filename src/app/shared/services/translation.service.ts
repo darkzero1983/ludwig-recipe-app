b686ff727d99cfda6bc4282218ef5e88c8576a14
Peter Ludwig <peter.ludwig@smarthouse.de>
2018-01-29 13:49:00 +0100

    CHANGED: translations

--- src/app/shared/services/translation.service.ts
+++ src/app/shared/services/translation.service.ts
@@ -15,13 +15,22 @@ export class TranslationService  {
         
     }
 
-    getTranslation(term: string, params: string[]): string
+    getTranslation(term: string, params?: string[]): string
     {
         let translation: Translation[] = this.transaltions.filter(x => x.Culture == this.currentCulture && x.Term == term); 
 
         if(translation != null && translation.length > 0)
         {
-            return translation[0].Translation;
+            let translationString: string = translation[0].Translation;
+            
+            if(params != null)
+            {
+                for (var i = 0; i < params.length; i++) {
+                    translationString = translationString.replace("{" + i + "}", params[i])
+                }
+            }
+
+            return translationString;
         }
         return "";
     }
@@ -31,6 +40,10 @@ export class TranslationService  {
         this.transaltions = new Array<Translation>();
         this.setTranslation("Account.Headline", "Einloggen", "Login");
         this.setTranslation("Account.UserName.Label", "Benutzername", "Username");
+        this.setTranslation("Account.UserName.Hint", "Max {0} Zeichen", "Max {0} Since");
+        this.setTranslation("Account.UserName.Error.Required", "Es muss ein Name eingetragen werden", "Name is required");
+        this.setTranslation("Account.UserName.Error.MaxLength", "Der Name ist zu lang (aktuell {0} von {1} Zeichen)", "Name is to (currently {0} of {1} since)");
+        
     }
 
     private setTranslation(term: string, german: string, english: string)
