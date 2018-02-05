db619176cad4148a4612da2305b6baa9652bc1de
Peter Ludwig <darkzero@gmx.com>
2018-02-05 20:48:58 +0100

    CHANGED: reorder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,21 @@
 export const environment = {
   production: true,
   useServiceWorker: true,
+  imageManagerDomain: '',
+
   useTestData: false,
+  
+  //API Urls
   apiNavigation: "/test_data/navigation.json",
-  apiNavigationTest: "/test_data/navigation.json",
-  apiRecipeSearch: "https://www.ludwigs-rezepte.de/api/Recipe/Search",
-  apiRecipeSearchTest: "/test_data/recipe-search-result.json",
+  apiRecipeSearch: "/test_data/recipe-search-result.json",
   apiAccountCanActive: "/test_data/canActivate.txt",
-  apiAccountCanActiveTest: "/test_data/canActivate.txt",
   apiAccountData: "/test_data/account-data.json",
-  apiAccountDataTest: "/test_data/account-data.json",
   apiRecipeOverview: "/api/Recipe/Overview",
-  apiRecipeOverviewTest: "/test_data/recipe-overview.json"
+
+  //Test URLs
+  apiNavigationTest: "/test_data/navigation.json",
+  apiRecipeSearchTest: "/test_data/recipe-search-result.json",
+  apiAccountCanActiveTest: "/test_data/canActivate.txt",
+  apiAccountDataTest: "/test_data/account-data.json",
+  apiRecipeOverviewTest: "/test_data/recipe-overview.json",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,21 @@
 export const environment = {
   production: false,
   useServiceWorker: true,
- 
+  imageManagerDomain: 'http://localhost:8005',
+
   useTestData: false,
+
+  //API Urls
   apiNavigation: "/test_data/navigation.json",
-  apiNavigationTest: "/test_data/navigation.json",
   apiRecipeSearch: "https://www.ludwigs-rezepte.de/api/Recipe/Search",
-  apiRecipeSearchTest: "/test_data/recipe-search-result.json",
   apiAccountCanActive: "/test_data/canActivate.txt",
-  apiAccountCanActiveTest: "/test_data/canActivate.txt",
   apiAccountData: "/test_data/account-data.json",
-  apiAccountDataTest: "/test_data/account-data.json",
   apiRecipeOverview: "http://localhost:8030/Recipe/Overview",
-  apiRecipeOverviewTest: "/test_data/recipe-overview.json"
+  
+  //Test URLs
+  apiNavigationTest: "/test_data/navigation.json",
+  apiRecipeSearchTest: "/test_data/recipe-search-result.json",
+  apiAccountCanActiveTest: "/test_data/canActivate.txt",
+  apiAccountDataTest: "/test_data/account-data.json",
+  apiRecipeOverviewTest: "/test_data/recipe-overview.json",
 };
